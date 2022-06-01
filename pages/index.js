cf2585849e250a56adf73e31ecdba1d387401aa6
Joshhortt <joshhortt@yahoo.com>
2022-06-01 18:53:06 +0100

    add button wrapper and styles

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,23 @@ import styled from 'styled-components'
 import { useWeb3 } from '@3rdweb/hooks'
 
 export default function Home() {
- 
+ // add button
     const { address, connectWallet } = useWeb3()
     return (
-      <div></div>
+      <Wrapper><button onClick={() => connectWallet('injected')}>
+      Connect Wallet
+    </button></Wrapper>
     )
   }
-  
+
+  // add styles
+
+  const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  max-width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  display: grid;
+  place-items: center;
+`
