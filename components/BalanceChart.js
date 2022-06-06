54ce2655e2fe6a33bd6e7735ba02d70cf49c2795
Joshhortt <joshhortt@yahoo.com>
2022-06-06 18:58:12 +0100

    add datasets, numbers data and color styles

--- components/BalanceChart.js
+++ components/BalanceChart.js
@@ -16,6 +16,29 @@ const data = {
       'Dec',
       'Jan',
     ],
+
+    datasets: [
+        {
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#3773f5',
+          borderColor: '#3773f5',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: '#3773f5',
+          pointBackgroundColor: '#3773f5',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: '#3773f5',
+          pointHoverBorderColor: '#3773f5',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 72, 45, 67, 55, 42],
+        },
+      ],
 }
 
 const BalanceChart = () => {
