35faeb19f395340af6fba62825d3982abf429679
Joshhortt <joshhortt@yahoo.com>
2022-06-06 18:56:25 +0100

    add data labels - all yearly months

--- components/BalanceChart.js
+++ components/BalanceChart.js
@@ -3,6 +3,21 @@ import styled from 'styled-components'
 import { Line } from 'react-chartjs-2'
 import Chart from 'chart.js/auto'
 
+const data = {
+    labels: [
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sept',
+      'Oct',
+      'Noz',
+      'Dec',
+      'Jan',
+    ],
+}
+
 const BalanceChart = () => {
   return (
     <Wrapper>
