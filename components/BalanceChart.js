c6c2f706c3286f34289ea364550054785de04938
Joshhortt <joshhortt@yahoo.com>
2022-06-06 18:41:09 +0100

    create BalanceChart component + chartjs dependency

---
+++ components/BalanceChart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Line } from 'react-chartjs-2'
+import Chart from 'chart.js/auto'
+
+const BalanceChart = () => {
+  return (
+    <div>BalanceChart</div>
+  )
+}
+
+export default BalanceChart
+
+const Wrapper = styled.div``
